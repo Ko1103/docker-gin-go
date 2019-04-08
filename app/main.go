f98856c218ec39dd0e89164ba65bcf2353e80745
Ko1103 <ko.foot1103@gmail.com>
2019-04-08 17:11:44 +0900

    add new app

---
+++ app/main.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+  "log"
+  "net/http"
+  "path/filepath"
+  "sync"
+  "text/template"
+)
+
+type templateHandler struct {
+  once sync.Once
+  filename string
+  templ *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  t.once.Do(func() {
+    t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+  })
+  t.templ.Execute(w, nil)
+}
+
+func main() {
+  http.Handle("/", &templateHandler{filename: "chat.html"})
+
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal("ListenAndServe", err)
+  }
+}
